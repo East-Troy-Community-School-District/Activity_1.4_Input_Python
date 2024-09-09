e621f5bf7c5ac272b17acc3779dd5a6e0f6739aa
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2024-09-09 13:57:11 -0500

    Add files via upload

---
+++ NumericInput.py
@@ -0,0 +1,8 @@
+# Numeric Input
+# 9/9/2024
+# Programming Fundamentals
+
+number1 = int(input("Enter a number >> "))
+number2 = int(input("Enter another number >> "))
+number_sum = number1 + number2
+print(str(number1) + " + " + str(number2) + " = " + str(number_sum))
---
+++ TextInput.py
@@ -0,0 +1,7 @@
+# TextInput
+# 9/9/2024
+# Programming Fundamentals
+
+name = input("What is your name? >> ")
+print("Hello, " + name + "!")
+# Add your code here!
