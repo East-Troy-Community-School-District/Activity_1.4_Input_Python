27b311844c876ef320c698aa4521dbefc9a49a27
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2024-09-17 20:57:41 -0500

    Add files via upload

---
+++ SquareArea.py
@@ -0,0 +1,8 @@
+# SquareArea
+# Pawelski
+# 9/17/2024
+# Programming Fundamentals
+
+side_length = input("Enter the side length of the square >> ")
+area = side_length * side_length
+print("The square has an area of " + str(area) + ".")
---
+++ Volume.py
@@ -0,0 +1,10 @@
+# Volume
+# Pawelski
+# 9/17/2024
+# Programming Fundamentals
+
+height = int(input("Enter the height of the box >> "))
+width = # write the code to ask the user to enter the width of the box
+# write the code to ask the user to enter the depth of the box and store it in a variable called depth
+# write the code to calculate the volume of the box and store it in a variable called volume
+print("volume = " + str(volume))
